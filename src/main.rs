3b8c80540c36e60877ae45fdcf8d5b629cb1f3c3
Jing Yen Loh <lohjingyen@gmail.com>
2023-07-31 00:00:31 +0800

    Execute commands naively

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() -> io::Result<()> {
     print!("$ ");
     io::stdout().flush()?;
 
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
 
-    println!("{}", buffer.trim());
+    let mut iter = line.split_whitespace();
+    let command = iter.next().unwrap();
+    let args: Vec<&str> = iter.collect();
+
+    let output = Command::new(command).args(args).output()?;
+    io::stdout().write_all(output.stdout.as_slice())?;
 
     main()
 }
