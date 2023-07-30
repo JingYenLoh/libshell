8d974fd2fc99af745cd82786ae5c637022ec44b2
Jing Yen Loh <lohjingyen@gmail.com>
2023-07-30 23:30:45 +0800

    Implement echo shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    print!("$ ");
+    io::stdout().flush()?;
+
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    println!("{}", buffer.trim());
+
+    main()
 }
